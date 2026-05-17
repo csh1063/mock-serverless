033a407026141dc2fc07892769f21ce16b2186ab
sanghyeon <csh1063@gmail.com>
2026-05-18 01:54:52 +0900

    response 수정

--- api/moa/contact/feedback.js
+++ api/moa/contact/feedback.js
@@ -31,5 +31,5 @@ export default async function handler(req, res) {
         return res.status(500).json({ error });
     }
 
-    return res.status(200).json({ result: true });
+    return res.status(200).json({ result: true, data:{} });
 }
